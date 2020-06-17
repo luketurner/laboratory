bcd341e02d1a043e56e15894aa3d09e2ad0e2bcd
Luke Turner <github@luketurner.org>
2020-06-17 15:57:03 -0700

    add `shell` and `kubectl` functions

--- laboratory/apis/kubecfg.py
+++ laboratory/apis/kubecfg.py
@@ -2,14 +2,16 @@ import subprocess
 import os.path
 from .. import AppException
 from ..config import get_manifest_directory
+from .shell import shell
 
 def kubecfg(manifest, args=None):
-  manifest_directory = get_manifest_directory()
-  manifest_path = os.path.normpath(os.path.join(manifest_directory, manifest))
+  manifest_path = os.path.normpath(os.path.join(get_manifest_directory(), manifest))
   cmd = ["kubecfg", "update", manifest_path]
   for k, v in (args or {}).items():
     cmd += ["--tla-str", k+"="+v]
-  exit_code = subprocess.call(cmd)
-  if exit_code > 0:
-    raise AppException("kubecfg returned exit code: {}".format(exit_code))
-  return exit_code
+  return shell(cmd)
+
+
+def kubectl(manifest):
+  manifest_path = os.path.normpath(os.path.join(get_manifest_directory(), manifest))
+  return shell(["kubectl", "apply", "-f", manifest_path])
