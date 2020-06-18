1f94fc0017c9d250512777ba06c19ae575079462
Luke Turner <github@luketurner.org>
2020-06-17 17:15:05 -0700

    Automatically use correct kubeconfig file

--- laboratory/apis/kubecfg.py
+++ laboratory/apis/kubecfg.py
@@ -1,17 +1,37 @@
 import subprocess
 import os.path
+import tempfile
+import contextlib
+
 from .. import AppException
-from ..config import get_manifest_directory
+from ..config import get_manifest_directory, get_cloud
 from .shell import shell
+
 
 def kubecfg(manifest, args=None):
   manifest_path = os.path.normpath(os.path.join(get_manifest_directory(), manifest))
-  cmd = ["kubecfg", "update", manifest_path]
-  for k, v in (args or {}).items():
-    cmd += ["--tla-str", k+"="+v]
-  return shell(cmd)
+  with kubeconfig() as kc:
+    cmd = ["kubecfg", "--kubeconfig", kc, "update", manifest_path]
+    for k, v in (args or {}).items():
+      cmd += ["--tla-str", k+"="+v]
+    return shell(cmd)
 
 
 def kubectl(manifest):
   manifest_path = os.path.normpath(os.path.join(get_manifest_directory(), manifest))
-  return shell(["kubectl", "apply", "-f", manifest_path])
+  with kubeconfig() as kc:
+    return shell(["kubectl", "--kubeconfig", kc,  "apply", "-f", manifest_path])
+
+@contextlib.contextmanager
+def kubeconfig():
+  cloud = get_cloud()
+
+  with tempfile.NamedTemporaryFile('w') as f:
+    # TODO -- abstract this better -- should not require hardcoded cloud type check
+    if cloud == "digitalocean":
+      from laboratory.actions.digitalocean.cluster import connect_cluster
+      f.write(connect_cluster())
+      f.flush()
+      yield f.name
+    else:
+      raise AppException("Cannot get kubeconfig for cloud: {}".format(cloud))
