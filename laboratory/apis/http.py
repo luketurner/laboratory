4bb145fef1e9578e95eec2827384fabecba5954f
Luke Turner <github@luketurner.org>
2021-07-31 11:42:40 -0700

    add types to download_file params

--- laboratory/apis/http.py
+++ laboratory/apis/http.py
@@ -1,6 +1,6 @@
 import requests
 
-def download_file(url, filename):
+def download_file(url: str, filename: str):
     resp = requests.get(url)
     with open(filename, 'wb') as fd:
         for chunk in resp.iter_content(chunk_size=128):
