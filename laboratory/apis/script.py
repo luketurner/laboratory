e24a3a43d0a924b99120736bdd8081df985696b4
Luke Turner <github@luketurner.org>
2021-07-30 18:44:32 -0700

    support running scripts with sudo

--- laboratory/apis/script.py
+++ laboratory/apis/script.py
@@ -3,5 +3,5 @@ import os.path
 from typing import List
 from .shell import shell
 
-def script(name: str, args: List[str]):
-    shell([os.path.join('scripts', name)] + args)
+def script(name: str, args: List[str], sudo = False):
+    return shell([os.path.join('scripts', name)] + args, sudo=sudo)
--- laboratory/apis/shell.py
+++ laboratory/apis/shell.py
@@ -4,8 +4,8 @@ from typing import List
 from ..errors import ApplicationException
 
 
-def shell(args: List[str]):
-    code = subprocess.call(args)
-    if code > 0:
-        raise ApplicationException("Shell command returned nonzero exit code {}: {}".format(code, " ".join(args)))
-    return code
+def shell(args: List[str], sudo = False):
+    result = subprocess.run(["sudo"] + args if sudo else args)
+    if result.returncode > 0:
+        raise ApplicationException("Shell command returned nonzero exit code {}: {}".format(result.returncode, " ".join(args)))
+    return result
