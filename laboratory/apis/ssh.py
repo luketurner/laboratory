b2153af3a02d1e596b345a6cd8f2ff3d361b2e29
Luke Turner <github@luketurner.org>
2021-08-22 20:15:42 -0700

    support capturing ssh output

--- laboratory/apis/ssh.py
+++ laboratory/apis/ssh.py
@@ -2,10 +2,12 @@ from paramiko.client import SSHClient, AutoAddPolicy
 from typing import List
 from ..config import get_in_config
 
-def ssh(host: str, cmds: List[List[str]], username: str):
+
+def ssh(host: str, cmds: List[List[str]], username: str, return_stdio=False):
     client = SSHClient()
     client.load_system_host_keys()
     client.set_missing_host_key_policy(AutoAddPolicy)
+    results = []
     try:
         print(f"executing commands on host: {username}@{host}")
         client.connect(
@@ -16,11 +18,18 @@ def ssh(host: str, cmds: List[List[str]], username: str):
         
         for cmd in cmds:
             cmdstr = " ".join(f"'{x}'" for x in cmd)
+            if len(cmdstr) > 120:
+                print(">", cmdstr[0:30], "... [SNIP] ...", cmdstr[-30:-1])
+            else:
             print(f"> {cmdstr}")
             stdin, stdout, stderr = client.exec_command(cmdstr)
+            if return_stdio:
+                results.append((stdout.read(), stderr.read()))            
+            else:
             for line in stdout:
                 print("    ", line, end="")
             for line in stderr:
                 print("  E ", line, end="")
+        return results
     finally:
         client.close()
