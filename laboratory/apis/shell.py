34da83c89777c8528594b6d149f64749a12a4ed0
Luke Turner <github@luketurner.org>
2021-08-22 20:14:56 -0700

    support capturing shell output

--- laboratory/apis/shell.py
+++ laboratory/apis/shell.py
@@ -4,8 +4,8 @@ from typing import List
 from ..errors import ApplicationException
 
 
-def shell(args: List[str], sudo = False):
-    result = subprocess.run(["sudo"] + args if sudo else args)
+def shell(args: List[str], sudo = False, capture_output = False):
+    result = subprocess.run(["sudo"] + args if sudo else args, capture_output=capture_output)
     if result.returncode > 0:
         raise ApplicationException("Shell command returned nonzero exit code {}: {}".format(result.returncode, " ".join(args)))
     return result
